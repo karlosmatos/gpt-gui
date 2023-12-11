48002e5bd871df68d48f5c14dfcfd5d26b4bb173
karlosmatos <kmkarelmaly@gmail.com>
2023-12-11 15:28:25 +0100

    Rafactor code and move constants to the class

--- app.py
+++ app.py
@@ -2,66 +2,65 @@ import os
 import streamlit as st
 from openai import OpenAI
 from dotenv import load_dotenv
+from typing import Tuple, Dict, Any
 
 load_dotenv()
 
-# Constants
-API_KEY = os.getenv('OPENAI_API_KEY')  # Replace with your API key
-MODEL_MAPPING = [
-    'gpt-3.5-turbo-1106',
-    'gpt-4-1106-preview'
+class GPTChat:
+    # Class-level constants
+    MODEL_MAPPING = [
+        'gpt-3.5-turbo-1106',
+        'gpt-4-1106-preview'
     ]
 
-COST_PER_TOKEN = {
-    'gpt-3.5-turbo-1106': {
-        'prompt': 0.001 / 1000,
-        'completion': 0.002 / 1000
-    },
-    'gpt-4-1106-preview': {
-        'prompt': 0.01 / 1000,
-        'completion': 0.03 / 1000
+    COST_PER_TOKEN = {
+        'gpt-3.5-turbo-1106': {
+            'prompt': 0.001 / 1000,
+            'completion': 0.002 / 1000
+        },
+        'gpt-4-1106-preview': {
+            'prompt': 0.01 / 1000,
+            'completion': 0.03 / 1000
+        }
     }
-}
-DEFAULT_MESSAGES = [
-    {
-        'role': 'system', 
-        'content': """
-        You are Senior Python Software Developer.
-        """
-    }
-]
 
-class WizardChat:
-    def __init__(self, api_key):
-        st.set_page_config(page_title='Chat', page_icon='')
+    DEFAULT_MESSAGES = [
+        {
+            'role': 'system',
+            'content': "You are Senior Python Software Developer."
+        }
+    ]
+
+    def __init__(self, api_key: str):
+        st.set_page_config(page_title='Chat', page_icon='💬')
         self.client = OpenAI(api_key=api_key)
         self.initialize_session_state()
 
     def initialize_session_state(self):
-        for key, value in {
+        initial_state = {
             'generated': [],
             'past': [],
-            'messages': DEFAULT_MESSAGES.copy(),
+            'messages': self.DEFAULT_MESSAGES.copy(),
             'model_name': [],
             'cost': [],
             'total_tokens': [],
             'total_cost': 0.0
-        }.items():
+        }
+        for key, value in initial_state.items():
             st.session_state.setdefault(key, value)
 
-    @staticmethod
-    def reset_conversation():
+    def reset_conversation(self):
         st.session_state.update({
             'generated': [],
             'past': [],
-            'messages': DEFAULT_MESSAGES.copy(),
+            'messages': self.DEFAULT_MESSAGES.copy(),
             'model_name': [],
             'cost': [],
             'total_tokens': [],
             'total_cost': 0.0
         })
 
-    def generate_response(self, prompt, model_name):
+    def generate_response(self, prompt: str, model_name: str) -> Tuple[str, Any]:
         st.session_state['messages'].append({'role': 'user', 'content': prompt})
 
         completion = self.client.chat.completions.create(
@@ -73,34 +72,33 @@ class WizardChat:
         st.session_state['messages'].append({'role': 'assistant', 'content': response})
 
         return response, completion.usage
-                        
-    # Add a new method to the WizardChat class
-    def process_user_input(self, user_input, model_name):
+
+    def process_user_input(self, user_input: str, model_name: str) -> Tuple[str, Any]:
         output, usage = self.generate_response(user_input, model_name)
         return output, usage
-    
-    @staticmethod
-    def calculate_cost(model_name, usage):
-        return (usage.prompt_tokens * COST_PER_TOKEN[model_name]['prompt'] +
-                usage.completion_tokens * COST_PER_TOKEN[model_name]['completion'])
+
+    def calculate_cost(self, model_name: str, usage: Any) -> float:
+        return (usage.prompt_tokens * self.COST_PER_TOKEN[model_name]['prompt'] +
+                usage.completion_tokens * self.COST_PER_TOKEN[model_name]['completion'])
 
     @staticmethod
-    def display_chat_history(response_container):
+    def display_chat_history(response_container: st.container):
         with response_container:
             for user_message, ai_message in zip(st.session_state['past'], st.session_state['generated']):
                 st.chat_message(name='user', avatar='🧑').markdown(user_message)
-                st.chat_message(name='ai', avatar='🧙‍♂️').markdown(ai_message)
+                st.chat_message(name='ai', avatar='🤖').markdown(ai_message)
 
     def chat_demo(self):
-        st.markdown('# Chat', unsafe_allow_html=True)
+        st.markdown('# Chat with GPT', unsafe_allow_html=True)
 
-        model_name = st.sidebar.radio('Choose a model:', list(MODEL_MAPPING))
+        model_name = st.sidebar.radio('Choose a model:', self.MODEL_MAPPING)
         counter_placeholder = st.sidebar.empty()
         counter_placeholder.write(f"Total cost of this conversation: ${st.session_state['total_cost']:.5f}")
         if st.sidebar.button('Clear Conversation', key='clear'):
             self.reset_conversation()
 
-        if (user_input := st.chat_input(placeholder='Type your message here...', key='input')):
+        user_input = st.chat_input(placeholder='Type your message here...', key='input')
+        if user_input:
             output, usage = self.process_user_input(user_input, model_name)
             st.session_state['past'].append(user_input)
             st.session_state['generated'].append(output)
@@ -115,5 +113,6 @@ class WizardChat:
         counter_placeholder.write(f"Total cost of this conversation: ${st.session_state['total_cost']:.5f}")
 
 if __name__ == '__main__':
-    wizard_chat = WizardChat(API_KEY)
-    wizard_chat.chat_demo()
+    api_key = os.getenv("OPENAI_API_KEY")  # Fetch the API key from environment variables
+    gpt_chat = GPTChat(api_key)
+    gpt_chat.chat_demo()
